0c97ba814006af6a1ff69bb26a762c68380f77ef
JerryWang-MSIS <Pw9@iu.edu>
2021-10-11 12:10:52 -0400

    Updated work based on lecture

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,61 +1,85 @@
 
-const SomeApp = {
+const Offer = {
   data() {
     return {
-      students: [],
-      selectedStudent: null,
-      offers: []
+      "students": [],
+      "selectedStudent": null,
+      "offers": []
     }
   },
-  computed: {},
-  methods: {
-      prettyData(d) {
-          return dayjs(d)
-          .format('D MMM YYYY')
+
+      computed: {
+        prettyData(d) {
+            return dayjs(d)
+            .format('D MMM YYYY')
+        }
       },
-      prettyDollar(n) {
-          const d = new Intl.NumberFormat("en-US").format(n);
-          return "$ " + d;
-      },
-      selectStudent(s) {
-          if (s == this.selectedStudent) {
-              return;
-          }
-          this.selectedStudent = s;
-          this.offers = [];
-          this.fetchOfferData(this.selectedStudent);
-      },
+
+  methods:{
+       selectStudent(s) {
+           if (s == this.selectedStudent) {
+               return;
+           }
+           this.selectedStudent = s;
+           this.offers = [];
+           this.fetchOfferData(s));
+       },
+
       fetchStudentData() {
           fetch('/api/student/')
-          .then( response => response.json() )
-          .then( (responseJson) => {
-              console.log(responseJson);
-              this.students = responseJson;
+          .then( response => response.json())
+          .then( (parsedJson) => {
+              console.log(parsedJson);
+              this.students = parsedJson;
           })
           .catch( (err) => {
               console.error(err);
           })
-      },
-      fetchOfferData(s) {
-          console.log("Fetching offer data for ", s);
-          fetch('/api/offer/?student=' + s.id)
-          .then( response => response.json() )
-          .then( (responseJson) => {
-              console.log(responseJson);
-              this.offers = responseJson;
-          })
-          .catch( (err) => {
-              console.error(err);
-          })
-          .catch( (error) => {
-              console.error(error);
-          });
       }
   },
+
   created() {
       this.fetchStudentData();
   }
+}
+
+//   computed: {},
+//   methods: {
+//       prettyData(d) {
+//           return dayjs(d)
+//           .format('D MMM YYYY')
+//       },
+//       prettyDollar(n) {
+//           const d = new Intl.NumberFormat("en-US").format(n);
+//           return "$ " + d;
+//       },
+//       selectStudent(s) {
+//           if (s == this.selectedStudent) {
+//               return;
+//           }
+//           this.selectedStudent = s;
+//           this.offers = [];
+//           this.fetchOfferData(this.selectedStudent);
+//       }
+
+         fetchOfferData(s) {
+             console.log("Fetching offer data for ", s);
+             fetch('/api/offer/?student=' + s.id)
+             .then( response => response.json() )
+             .then( (parsedJson) => {
+                 console.log(parsedJson);
+                 this.offers = parsedJson;
+             })
+             .catch( (error) => {
+                 console.error(error);
+             })
+       }
+   },
+
+   created() {
+       this.fetchStudentData();
+   }
 
 }
 
-Vue.createApp(SomeApp).mount('#offerApp');
+ Vue.createApp(SomeApp).mount('#offerApp');  
